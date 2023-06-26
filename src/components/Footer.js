e7269251e4a9e37c2aa13c29b7cd38d476879557
Sondre Alfnes <sondre.alfnes@gmail.com>
2023-06-27 00:19:10 +0200

    New portfolio

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div
+              className="social-icon"
+              style={{
+                marginTop: "20px",
+              }}
+            >
+              <a href="https://www.linkedin.com/in/sondre-alfnes-2243331b3/">
+                <img src={navIcon1} alt="" />
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
